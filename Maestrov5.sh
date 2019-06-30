9ce8b36d35094f25df3bf1dd387812b3c9c9239f
MaestroTool <51473403+MaestroTool@users.noreply.github.com>
2019-06-30 17:28:17 +0700

    Memek

---
+++ Maestrov5.sh
@@ -0,0 +1,190 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+echo
+figlet Maestro
+echo ""
+echo
+echo -n $me" Masukkan Nama Anda :"
+read a
+if [ ! -z  $a ]
+then
+echo $bi"Hai "$me" $a "
+fi
+sleep 5
+echo $i"Ini adalah Username dan Password anda"
+echo $pu"Username :"$me" Maestro"
+echo $pu"Password :"$me" Tamvan"
+sleep 10
+python2 Memek.py
+echo
+clear
+echo
+echo $red"          Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa anjing :v("   
+sleep 1
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+sleep 1
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+sleep 1
+clear
+sleep 1
+clear
+echo
+sleep 4
+clear
+figlet Maestro
+echo $ku"["$me"⚠️"$ku"]"$i"▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️"$ku"["$me"⚠️"$ku"]"
+echo $bi"["$me"+"$bi"]"$pu" 🔱Pencipta :"$me"  MAESTRO"
+echo $bi"["$me"+"$bi"]"$pu" 🔱Facebook :"$me"  www.facebook.com/maestro10001"
+echo $bi"["$me"+"$bi"]"$pu" 🔱Type     :"$me"  Tool Installer"
+echo $bi"["$me"+"$bi"]"$pu" 🔱Version  :"$me"  v1.0"
+echo $ku"["$me"⚠️"$ku"]"$i"▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️"$ku"["$me"⚠️"$ku"]"
+sleep 2
+			             
+echo " "
+echo "🔰"$cy"["$me"MENU"$cy"]"$me"🔰"
+sleep 1
+echo "⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩ ⏩⏩⏩⏩⏩⏩⏩⏩"
+echo $i"["$me"1"$i"]"$bi"🔀🔀|DarkFB v1.6"$me"                    [M]"
+sleep 2
+echo $i"["$me"2"$i"]"$bi"🔀🔀|DarkFB Peremuim"$me"                [A]"
+sleep 0.2
+echo $i"["$me"3"$i"]"$bi"🔀🔀|REPORT FB"$me"	        	    [E]"
+sleep 0.3
+echo $i"["$me"4"$i"]"$bi"🔀🔀|BRUTE FORCE TAGET"$me"			  [S]"
+sleep 0.4
+echo $i"["$me"5"$i"]"$bi"🔀🔀|BRUTE FORCE FB CRACKER"$me"		  [T]"
+sleep 0.1
+echo $i"["$me"6"$i"]"$bi"🔀🔀|MBF"$me"					  [R]"
+sleep 0.1
+echo $i"["$me"7"$i"]"$bi"🔀🔀|CLONING YAHOO"$me"			  [O]"
+sleep 0.1
+echo $i"["$me"8"$i"]"$bi"🔀🔀|OSIF"$me"				  [T]"
+sleep 0.1
+echo $i"["$me"9"$i"]"$bi"🔀🔀|PROFILEGUARD FB"$me"		  [O]"
+sleep 0.1
+echo $i"["$me"10"$i"]"$bi"🔀 |BOT REACTION FACEBOOK"$me"	  [L]"
+sleep 0.1
+echo $i"["$me"00"$i"]"$pu" EXIT"
+echo "⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩"
+sleep 0.1
+echo 
+read -p"[########] : " pil
+			                      		
+if [ $pil = 1 ]
+then
+clear
+sleep 1
+git clone https://github.com/mrb4n6547/Bangsad.git
+cd Bangsad $y
+python2 BgSdKw.py $y
+fi
+
+if [ $pil = 2 ]
+then
+clear
+sleep 1
+git clone https://github.com/MaestroTool/DarkFBP.git
+cd DarkFBP $y
+python2 BGSD.py $y
+fi
+	     
+if [ $pil = 3 ]
+then
+clear
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report $y
+unzip Report.zip $y
+python2 Report.py $y
+fi
+
+if [ $pil = 4 ]
+then
+clear
+sleep 1
+git clone https://github.com/MaestroTool/HackFB.git
+cd HackFB $y
+python2 hack.py $y
+fi
+
+if [ $pil = 5 ]
+then
+clear
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
+cd Fb-Cracker-v.3 $y
+python2 crack.py $y
+fi
+
+if [ $pil = 6 ]
+then
+clear
+sleep 1
+git clone https://github.com/MaestroTool/TolBgsD.git
+cd TolBgsD $y
+python2 MBFUCK.py $y
+fi
+
+if [ $pil = 7 ]
+then
+clear
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning $y
+pip2 install requests mechanize $y
+pip2 install requirements $y
+python2 cloning.py $y
+fi
+
+if [ $pil = 8 ]
+then
+clear
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF $y
+pip2 install -r requirements.txt $y
+python2 osif.py $y
+fi
+
+if [ $pil = 9 ]
+then
+clear
+sleep 1
+git clone https://github.com/MaestroTool/GuardAsw.git
+cd GuardAsw $y
+php GuardFBAsw.php $y
+fi
+
+if [ $pil = 10 ]
+then
+clear
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF $y
+pip2 install -r requirements.txt $y
+python2 osif.py $y
+fi
+
+if [ $pil = 00 ]
+then
+clear
+echo "Keluar"
+exit
+fi
+
